1f37284ef1ac509b6034e22578a43f8e8785df0a
kidgokugoku <g13728497910@gmail.com>
2022-02-14 15:01:22 +0800

    cmt

--- srt2ass.py
+++ srt2ass.py
@@ -4,14 +4,21 @@
 # by: ewwink
 #
 
-import sys
+import argparse
+import codecs
 import os
 import re
-import codecs
+import sys
+from ast import Store
+from concurrent.futures import process
 from glob import glob
 
+import opencc
+
+
 def fileopen(input_file):
-    encodings = ["utf-32", "utf-16", "utf-8", "cp1252", "gb2312", "gbk", "big5"]
+    encodings = ["utf-32", "utf-16", "utf-8",
+                 "cp1252", "gb2312", "gbk", "big5"]
     tmp = ''
     for enc in encodings:
         try:
@@ -24,13 +31,16 @@ def fileopen(input_file):
     return [tmp, enc]
 
 
-def srt2ass(input_file):
-    if '.ass' in input_file:
-        return input_file
+def srt2ass(input_file, en=False):
+    if not '.srt' in input_file:
+        print('input is not .srt file')
+        return
 
     if not os.path.isfile(input_file):
-        print (input_file + ' not exist')
+        print(input_file + ' not exist')
         return
+
+    print('processing:'+input_file)
 
     src = fileopen(input_file)
     tmp = src[0]
@@ -41,7 +51,7 @@ def srt2ass(input_file):
     if u'\ufeff' in tmp:
         tmp = tmp.replace(u'\ufeff', '')
         utf8bom = u'\ufeff'
-    
+
     tmp = tmp.replace("\r", "")
     lines = [x.strip() for x in tmp.split("\n") if x.strip()]
     subLines = ''
@@ -49,7 +59,7 @@ def srt2ass(input_file):
     lineCount = 0
     output_file = '.'.join(input_file.split('.')[:-1])
     output_file += '.ass'
-    output_file = re.sub(r'_track[\s\S]*]','',output_file)
+    output_file = re.sub(r'_track[\s\S]*]', '', output_file)
 
     for ln in range(len(lines)):
         line = lines[ln]
@@ -62,7 +72,10 @@ def srt2ass(input_file):
         else:
             if re.match('-?\d\d:\d\d:\d\d', line):
                 line = line.replace('-0', '0')
-                tmpLines += 'Dialogue: 0,' + line + ',eng,,0,0,0,,'
+                if en is True:
+                    tmpLines += 'Dialogue: 0,' + line + ',eng,,0,0,0,,'
+                else:
+                    tmpLines += 'Dialogue: 0,' + line + ',Default,,0,0,0,,'
             else:
                 if lineCount < 2:
                     tmpLines += line
@@ -71,7 +84,6 @@ def srt2ass(input_file):
             lineCount += 1
         ln += 1
 
-
     subLines += tmpLines + "\n"
 
     subLines = re.sub(r'\d(\d:\d{2}:\d{2}),(\d{2})\d', '\\1.\\2', subLines)
@@ -79,10 +91,12 @@ def srt2ass(input_file):
     # replace style
     subLines = re.sub(r'<([ubi])>', "{\\\\\g<1>1}", subLines)
     subLines = re.sub(r'</([ubi])>', "{\\\\\g<1>0}", subLines)
-    subLines = re.sub(r'<font\s+color="?#(\w{2})(\w{2})(\w{2})"?>', "{\\\\c&H\\3\\2\\1&}", subLines)
+    subLines = re.sub(
+        r'<font\s+color="?#(\w{2})(\w{2})(\w{2})"?>', "{\\\\c&H\\3\\2\\1&}", subLines)
     subLines = re.sub(r'</font>', "", subLines)
 
-    head_str = '''[Script Info]
+    if en is True:
+        head_str = '''[Script Info]
 ; This is an Advanced Sub Station Alpha v4+ script.
 Title:
 ScriptType: v4.00+
@@ -90,7 +104,19 @@ Collisions: Normal
 PlayDepth: 0
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: eng,Verdana,18,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,0,0,0,0,90,100,0,0,1,0.1,1,2,10,10,10,1
+Style: eng,Verdana,18,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,0,0,0,0,90,100,0,0,1,0.1,2,2,10,10,30,1
+[Events]
+Format: Layer, Start, End, Style, Actor, MarginL, MarginR, MarginV, Effect, Text'''
+    else:
+        head_str = '''[Script Info]
+; This is an Advanced Sub Station Alpha v4+ script.
+Title:
+ScriptType: v4.00+
+Collisions: Normal
+PlayDepth: 0
+[V4+ Styles]
+Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
+Style: Default,Droid Sans Fallback,23,&H00AAE2E6,&H00FFFFFF,&H00000000,&H00000000,-1,0,0,0,88,100,0,0,1,0.1,2,2,10,10,10,1
 [Events]
 Format: Layer, Start, End, Style, Actor, MarginL, MarginR, MarginV, Effect, Text'''
 
@@ -105,13 +131,39 @@ Format: Layer, Start, End, Style, Actor, MarginL, MarginR, MarginV, Effect, Text
     return output_file
 
 
-if len(sys.argv) > 1:
-    filelist = glob(sys.argv[1])
-    for arg in filelist:
-        print(arg)
-        srt2ass(arg)
-else:
-    filelist = glob('*.srt')
-    for arg in filelist:
-        print(arg)
-        srt2ass(arg)
+def loadArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file',
+                        help='srt file location',
+                        nargs='*',
+                        default='*.srt')
+    parser.add_argument("-e", "--english", "-en",
+                        help="handle only ENG subtitles",
+                        action='store_true')
+
+    global Args
+    Args = parser.parse_args()
+
+
+def main():
+    loadArgs()
+
+    print(Args)
+
+    file = Args.file
+    isEnglish = Args.english
+
+    if type(file) is list:
+        for arg in file:
+            srt2ass(arg, isEnglish)
+    else:
+        if '*' in file:
+            filelist = glob(file)
+            for arg in filelist:
+                srt2ass(arg, isEnglish)
+        else:
+            srt2ass(file, isEnglish)
+
+
+if __name__ == '__main__':
+    main()
